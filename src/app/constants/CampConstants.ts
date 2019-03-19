5fbb08968a8e649ca91f44f5b5ec732f61d60e3c
solidperez <hitgoal312@gmail.com>
2019-03-19 04:36:24 +0000

    Merged PR 168: Updated search category dropdown and added video to homepage
    Updated search category dropdown and added video to homepage

--- src/app/constants/CampConstants.ts
+++ src/app/constants/CampConstants.ts
@@ -2,16 +2,17 @@
 export class CampConstants {
 
     public CAMP_CATEGORY = [
-                                {"name" : "Science" , "id" :  10000},
-                                {"name" : "STEAM" , "id" :  11000},
-                                {"name" : "Sports" , "id" :  12000},
-                                {"name" : "Adventure" , "id" :  13000},
-                                {"name" : "Theatre" , "id" :  14000},
-                                {"name" : "Technology" , "id" :  15000},
-                                {"name" : "Arts" , "id" :  16000},
-                                {"name" : "Chess" , "id" :  17000},
-                                {"name" : "Lego" , "id" :  18000}
-                            ];
+        {"name" : "Adventure" , "id" :  13000},
+        {"name" : "Arts" , "id" :  16000},
+        {"name" : "Chess" , "id" :  17000},
+        {"name" : "Lego" , "id" :  18000}
+        {"name" : "Science" , "id" :  10000},
+        {"name" : "Sports" , "id" :  12000},
+        {"name" : "STEAM" , "id" :  11000},
+        {"name" : "Technology" , "id" :  15000},
+        {"name" : "Theatre" , "id" :  14000},
+        
+    ];
 
 }
 
