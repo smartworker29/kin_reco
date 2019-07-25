175fb7bd201c605f4a932c9f48f6b9e6d009febc
Luong Nguyen Dinh <luongnd169@gmail.com>
2019-07-25 15:22:32 +0700

    remove unused code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0Config: {
+    domain: 'dev--ilicir1.auth0.com',
+    client_id: 'SK978DumFV6PAhxKNFgM0g8vkZoLoxUN',
+    redirect_uri: `${window.location.origin}/callback`
+  }
 };
