1f5720084064e29039a037fd57cb4614137cb55b
solidperez <hitgoal312@gmail.com>
2020-01-30 21:19:15 +0000

    Merged PR 371: Classes actions
    Classes actions
    
    Cherry-picked from commit `6a722a50`.

--- src/app/venue/masonsry-venues-view/masonsry-venues-view.component.ts
+++ src/app/venue/masonsry-venues-view/masonsry-venues-view.component.ts
@@ -7,6 +7,8 @@ import { AuthService } from '@shared/service/auth.service';
 import { Observable } from 'rxjs';
 import { BsModalRef } from 'ngx-bootstrap';
 import { MatDialog } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { API_URL } from '@shared/constants/UrlConstants';
 declare let ga: any;
 
 @Component({
@@ -28,6 +30,7 @@ export class MasonsryVenuesViewComponent implements OnInit {
   @Input() end;
 
   constructor(private router: Router,
+              private http: HttpClient,
               private venuesService:VenuesService,
               private reviewService: ReviewsService,
               private authService: AuthService,
@@ -56,7 +59,7 @@ export class MasonsryVenuesViewComponent implements OnInit {
     this.router.navigate(['/venues', id]);
   }
 
-  add_subscription_venue(id,i) {
+  add_subscription_venue(id, i, venue_category) {
     const input_data = {
       "venue_subs_data": {
         "venue_id": id,
@@ -65,11 +68,12 @@ export class MasonsryVenuesViewComponent implements OnInit {
     this.venuesService.add_subscriptions(input_data).subscribe(data => {
       if (data['status'] === true) {
         this.venues[i].is_followed = "true";
+        this.add_analytics_data(id, i, 'SUBSCRIBE', venue_category);
       } else {
-        alert('Something went wrong while subscribe venue');
+        //alert('Something went wrong while subscribe venue');
       }
     }, error => {
-      alert('Something went wrong while subscribe venue');
+      //alert('Something went wrong while subscribe venue');
     });
 }
 
@@ -83,11 +87,12 @@ unsubscribe_venue(id,i) {
     });
 }
 
-save_camp(id, i) {
-  this.add_analytics_data(id, i, 'SAVE');
+save_camp(id, i, venue_category) {
+  this.add_analytics_data(id, i, 'SAVE', venue_category);
   this.venues[i].is_saved = "true";
 }
-add_analytics_data(id, i, atype: any) {
+
+add_analytics_data(id, i, atype: any, venue_category) {
   let action = '';
   switch (atype) {
     case 'CLICK':
@@ -96,6 +101,9 @@ add_analytics_data(id, i, atype: any) {
     case 'SAVE':
       action = ACTION.SAVE;
       break;
+    case 'SUBSCRIBE':
+      action = ACTION.SUBSCRIBE;
+      break;
     case 'CALENDAR':
       action = ACTION.CALENDAR;
       break;
@@ -103,19 +111,25 @@ add_analytics_data(id, i, atype: any) {
   let analytics_input = {};
   analytics_input = {
     'input_data': [{
-      'entity_type': ANALYTICS_ENTITY_TYPES_ENUM.VENUE,
+      'entity_type': (venue_category && venue_category[0] === 'Classes') ? 
+              ANALYTICS_ENTITY_TYPES_ENUM.Class : ANALYTICS_ENTITY_TYPES_ENUM.VENUE,
       'entity_id': id,
       'interface': INTERFACE_ENUM.FE,
       'action': action,
       'referrer': '/root/home'
     }]
   }
-  this.reviewService.add_analytics_actions(analytics_input).subscribe(data => {
+  if (!this.isLogedin) {
+    this.http.post(API_URL + 'actions/' , analytics_input).subscribe(data => {
+    });
+  } else {
+    this.reviewService.add_analytics_actions(analytics_input).subscribe(data => {
     if (atype === 'CLICK') {
       this.is_save_action(id, i);
     }
-  }, error => {
-  });
+    }, error => {
+    });
+  }
 
 }
 
@@ -148,11 +162,12 @@ deleteUser(linkName) {
 }
 
 //this function will open a popup when user is not loggen in
-checklogin(id, i, linkName) {
+checklogin(id, i, linkName, venue_category, atype) {
   if (this.isLogedin) {
     this.ClickName = linkName;
     // this.save_camp(id, i);
   } else {
+    this.add_analytics_data(id, i, atype, venue_category)
     this.deleteUser(linkName);
   }
 }
